9e288c75f16ab9281711ce9359e1cee0e965667d
lubien <lubien1996@gmail.com>
2017-03-02 18:31:35 -0300

    Init

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Cerebro Hex
+
+> A Cerebro plugin for searching hex.pm packages
+
+## Usage
+
+In Cerebro, type `hex packagename` to search for `packagename`
+
+You can:
+
+* Press Enter to open it's URL
+* Press Tab to change the current input to it's URL
+* Press <kbd>cmd+c</kbd> to copy it's URL to clipboard
+
+![](screenshot.png)
+
+## Related
+
+* [Cerebro](http://github.com/KELiON/cerebro) – main repo for Cerebro app;
+
+## License
+
+MIT © [Lubien](http://lubien.me)
---
+++ src/index.js
@@ -0,0 +1,66 @@
+'use strict';
+const {memoize} = require('cerebro-tools')
+const {compareTwoStrings} = require('string-similarity')
+const icon = require('../icon.png')
+const keyword = 'hex'
+const endpoint = 'https://hex.pm/api/packages/'
+const TAB_KEY = 9
+const memoizationSettings = {
+  maxAge: 60 * 1000 // 1 minute
+}
+
+const map = f => xs => xs.map(f)
+
+const sort = f => xs => xs.sort(f)
+
+const fetchAll = memoize(name =>
+  fetch(`${endpoint}?search=${encodeURIComponent(name)}`)
+    .then(response => response.json())
+, memoizationSettings)
+
+const packageToResult = actions => ({name, meta}) => {
+  const url = `https://hex.pm/packages/${name}`
+
+  return {
+    icon,
+    id: `hex-${name}`,
+    title: name,
+    subtitle: meta.description,
+    clipboard: url,
+    onSelect() {
+      actions.open(url)
+    },
+    onKeyDown(event) {
+      if (event.keyCode === TAB_KEY) {
+        actions.replaceTerm(url)
+      }
+    }
+  }
+}
+
+const sortBestMatch = name => (a, b) => {
+  const [matchA, matchB] = [
+    compareTwoStrings(name, a.name),
+    compareTwoStrings(name, b.name)
+  ]
+
+  return matchB - matchA
+}
+
+const plugin = ({term, display, update, actions}) => {
+  const rgx = /^hex\s+(.*)/
+
+  if (!rgx.test(term)) return
+
+  const [, name] = rgx.exec(term)
+
+  fetchAll(name)
+    .then(sort(sortBestMatch(name)))
+    .then(map(packageToResult(actions)))
+    .then(display)
+};
+
+module.exports = {
+  keyword,
+  fn: plugin
+}
